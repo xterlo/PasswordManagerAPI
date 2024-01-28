e0096f7f45cccf7219fc111812404c66b8a6ad83
x x <xterlo@yandex.ru>
2024-01-28 20:04:00 +0300

    +add Logger Functions

---
+++ Core/Logger.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PasswordManagerAPI.Database;
+
+namespace PasswordManagerAPI.Core
+{
+    public class Logger
+    {
+        public static void NewLog(string message,string sessionToken)
+        {
+
+            using(ApplicationDBContext db = new ApplicationDBContext())
+            {
+                var session = db.session.Include(x => x.User).Where(x => x.JWT == sessionToken);
+                if(sessionToken is null) return;        
+                if(session.Count() == 0) return;
+
+
+                db.Add(new Tables.Logger()
+                {
+                    SessionID = session.First().Id,
+                    Data = message,
+                    Date = DateTime.Now,
+                    UserID = session.First().UserId,
+                });
+                db.SaveChanges();
+            }
+        }
+    }
+}
